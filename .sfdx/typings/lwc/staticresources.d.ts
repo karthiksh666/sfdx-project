4ab5783032f2458a9d9da2bb4d3ee1a146280557
= <=>
2020-07-19 16:21:11 +0530

    C

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
+declare module "@salesforce/resourceUrl/ursus_park" {
+    var ursus_park: string;
+    export default ursus_park;
+}
